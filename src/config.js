74a77d80a171f14f5619c93ae6eb3d9a9024ccba
Fariduddin khan <fariduddinkhan7860@gmail.com>
2025-09-27 16:14:05 +0530

    Update dashboard URL in config.js to deployed domain

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ export const BACKEND_URL = "https://zerodha-clone-ljxo.onrender.com";
 export const FRONTEND_URL = "https://zerodha-clone-frontend-theta.vercel.app";
 
 //
-export const DASHBOARD_URL = "https://zerodha-dashboard.vercel.app";
+export const DASHBOARD_URL = "https://zerodha-clone-dashboard-pink.vercel.app";
