c968c5508a2a6230fb7bb167cd1c5b5b5b57f9c0
Fariduddin khan <fariduddinkhan7860@gmail.com>
2025-09-27 15:58:27 +0530

    Updated FRONTEND_URL with deployed URL

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 export const BACKEND_URL = "https://zerodha-clone-ljxo.onrender.com";
 
 //
-export const FRONTEND_URL = "https://your-frontend.vercel.app";
+export const FRONTEND_URL = "https://zerodha-clone-frontend-theta.vercel.app";
 
 //
 export const DASHBOARD_URL = "https://zerodha-dashboard.vercel.app";
